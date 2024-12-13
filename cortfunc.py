601786e49bfd79d1c5947808c21bf8cee302e446
Arseniy2059 <semyashulkovy64@mail.ru>
2024-12-13 21:28:54 +0300

    Update cortfunc.py

--- cortfunc.py
+++ cortfunc.py
@@ -1,26 +1,79 @@
-numc = [5, 6, 2, 1, 3, 4]
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
 
-def bubble_sort(ls):
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(len(ls) - 1):
-            if ls[i] > ls[i+1]:
-                ls[i+1], ls[i+1] = ls[i+1], ls[i]
-                swapped = True
+api = ''
 
-
-#bubble_sort(numc)
-#print(numc)
-
-def salection_sort(ls):
-    for i in range(len(ls)):
-        lowest = i
-        for j in range(i + 1, len(ls)):
-            if i in ls[j] < ls[lowest]:
-                lowest = j
-            ls[i], ls[lowest] = ls[lowest], ls[i]
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-salection_sort(numc)
-print(numc)
+kb = ReplyKeyboardMarkup()
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+
+kb.add(button1)
+kb.add(button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Рассчитать')],
+        [
+            KeyboardButton(text='Информация')
+        ]
+    ], resize_keyboard=True
+)
+
+
+@dp.message_handler(text=['Привет!'])
+async def start_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет я Бот с Информацией о себе', reply_markup=start_menu)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+        await state.update_data(age=message.text)
+        await message.answer(f"Введите свой рост:")
+        await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_growth(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer(f"Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    dta = await state.get_data()
+    m = 10*int(dta['weight'])
+    f = 6.25*int(dta['growth'])
+    r = 5*int(dta['age'])
+    mfr = (m + f - r) + 5
+    await message.answer(f'Ваша норма колорий {mfr}')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
